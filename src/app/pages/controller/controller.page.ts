fc0bfe9b6524d88957b9f599b68aba1c56c54b34
Diego <gomezefdiego@gmail.com>
2020-04-19 21:37:45 -0600

    Data table, missing functions

--- src/app/pages/controller/controller.page.ts
+++ src/app/pages/controller/controller.page.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./controller.page.scss'],
 })
 export class ControllerPage implements OnInit {
+  color="orange";
+  mode="Derribar";
+  left_icon="fas fa-level-down-alt";
+  right_icon="fab fa-jedi-order";
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  accion(a){
+    console.log(a);
+    switch(a){
+      case "1":
+        this.color="orange";
+        this.mode="Derribar";
+        this.left_icon="fas fa-level-down-alt";
+        this.right_icon="fab fa-jedi-order";
+        break;
+      case "2":
+        this.left_icon="fas fa-hand-paper";
+        this.right_icon="fas fa-biohazard";
+        this.mode="Detener";
+        this.color="dark";
+        break;
+      case "3":
+        this.left_icon="fas fa-route";
+        this.right_icon="fas fa-tachometer-alt";
+        this.mode="Evitar";
+        this.color="yellow";
+        break;
+      case "4":
+        this.left_icon="fas fa-radiation";
+        this.right_icon="fas fa-skull-crossbones";
+        this.mode="Atacar";
+        this.color="red";
+        break;
+    }
+  }
+
 
 }
--- src/app/pages/data/data.page.ts
+++ src/app/pages/data/data.page.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataPage implements OnInit {
 
+  //   Velocidad
+  // b. Distancia recorrida
+  // c. Tiempo total del viaje
+  // d. Cantidad de objetos evadidos
+  // e. Tiempo de toma de decisión
+
+  binnacle = [
+    {
+      speed:100,
+      distance:10,
+      total_time:35,
+      evaded_objects:3,
+      decision_time:10
+    },
+    {
+      speed:200,
+      distance:15,
+      total_time:25,
+      evaded_objects:2,
+      decision_time:4
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    this.mixItAll();
+    console.log(this.binnacle);
+  }
+
+  mixItAll(){
+    for(let x=0; x<=45; x++){
+      let newbinnacle ={
+        speed:this.getRandomInt(100,200),
+        distance:this.getRandomInt(0,20),
+        total_time:this.getRandomInt(5,60),
+        evaded_objects:this.getRandomInt(1,10),
+        decision_time:this.getRandomInt(6,30)
+      };
+      this.binnacle.push(newbinnacle);
+    }
+  }
+
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
 }
